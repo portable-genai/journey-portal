portable-genai/itsm-servicedesk-triage

Make customer-managed encryption optional, and off by default
Every binding to the stack's Cloud KMS key now sits behind cmek_enabled,
default false: the key ring, the key, every grant to a service agent, and
every store, log bucket, registry and revision that took the key. Block
bindings became dynamic blocks; scalar bindings read the key through one(),
which is null when the key does not exist and omits the attribute rather
than pointing it at nothing. The Terraform tests that assert on key material
turn the gate on to keep meaning what they meant.

Why the default is off: a key ring can never be deleted, a log bucket that
has CMEK can never drop it, and registries and document stores take their
key at creation. None of that changes an answer or a screen, and everything
is encrypted at rest with Google-managed keys regardless. A deployment with
a customer whose data it must shred, whose key access must be audited or
whose keys must live in an HSM sets it true in its own tfvars BEFORE the
first apply. Where a stack already applied it, the key's prevent_destroy
refuses a plan that omits the line, which is the right answer: the stores it
bound stay bound.

The vendored deploy-and-residency-hardening skill says the same.

infra/terraform/kms.tf
@@ -16,6 +16,7 @@
 # NOTE for template maintainers: copied into a render VERBATIM. No Jinja here.
 
 resource "google_kms_key_ring" "cmek" {
+  count    = var.cmek_enabled ? 1 : 0
   name     = local.kms_ring_name
   location = local.region # regional, in-country key material (P-03)
 
@@ -23,8 +24,9 @@ resource "google_kms_key_ring" "cmek" {
 }
 
 resource "google_kms_crypto_key" "cmek" {
+  count    = var.cmek_enabled ? 1 : 0
   name     = local.kms_key_name
-  key_ring = google_kms_key_ring.cmek.id
+  key_ring = one(google_kms_key_ring.cmek[*].id)
 
   purpose         = "ENCRYPT_DECRYPT"
   rotation_period = "7776000s" # 90 days
@@ -58,14 +60,16 @@ data "google_project" "this" {
 
 # Cloud Logging, for the locked WORM audit bucket (logging_worm.tf).
 resource "google_kms_crypto_key_iam_member" "logging" {
-  crypto_key_id = google_kms_crypto_key.cmek.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.cmek[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gcp-sa-logging.iam.gserviceaccount.com"
 }
 
 # Cloud Run, for the serving revision's own encrypted storage (production_edge.tf).
 resource "google_kms_crypto_key_iam_member" "run" {
-  crypto_key_id = google_kms_crypto_key.cmek.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.cmek[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@serverless-robot-prod.iam.gserviceaccount.com"
 }
@@ -73,7 +77,8 @@ resource "google_kms_crypto_key_iam_member" "run" {
 # Vertex AI, for whatever narration or classification surface this vertical binds. The binding
 # is in the baseline so the key is already bound on the day the adapter arrives.
 resource "google_kms_crypto_key_iam_member" "aiplatform" {
-  crypto_key_id = google_kms_crypto_key.cmek.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.cmek[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gcp-sa-aiplatform.iam.gserviceaccount.com"
 }
@@ -82,7 +87,8 @@ resource "google_kms_crypto_key_iam_member" "aiplatform" {
 # bucket is per-repo, the key binding is not the interesting part of that commit, and a bucket
 # created without it is silently encrypted under Google-managed keys.
 resource "google_kms_crypto_key_iam_member" "storage" {
-  crypto_key_id = google_kms_crypto_key.cmek.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.cmek[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = "serviceAccount:service-${data.google_project.this.number}@gs-project-accounts.iam.gserviceaccount.com"
 }

infra/terraform/outputs.tf
@@ -26,7 +26,7 @@ output "residency_allowlist" {
 # --------------------------------- KMS -------------------------------------- #
 output "kms_key" {
   description = "Regional CMEK crypto key id, bound to Cloud Logging, Cloud Run, Vertex AI and Cloud Storage service agents."
-  value       = google_kms_crypto_key.cmek.id
+  value       = one(google_kms_crypto_key.cmek[*].id)
 }
 
 # ------------------------------- WORM logging ------------------------------- #

portable-genai/journey-portal

infra/terraform/kms.tf
@@ -1,4 +1,5 @@
 resource "google_kms_key_ring" "portal" {
+  count    = var.cmek_enabled ? 1 : 0
   project  = var.project_id
   name     = "${var.name_prefix}-portal"
   location = var.region
@@ -8,8 +9,9 @@ resource "google_kms_key_ring" "portal" {
 }
 
 resource "google_kms_crypto_key" "portal" {
+  count                      = var.cmek_enabled ? 1 : 0
   name                       = "${var.name_prefix}-portal"
-  key_ring                   = google_kms_key_ring.portal.id
+  key_ring                   = one(google_kms_key_ring.portal[*].id)
   rotation_period            = var.cmek_rotation_period
   destroy_scheduled_duration = "2592000s"
 
@@ -23,7 +25,8 @@ locals {
 }
 
 resource "google_kms_crypto_key_iam_member" "cloud_run" {
-  crypto_key_id = google_kms_crypto_key.portal.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.portal[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   member        = local.cloud_run_service_agent
 }
@@ -36,7 +39,8 @@ data "google_logging_project_cmek_settings" "portal" {
 }
 
 resource "google_kms_crypto_key_iam_member" "logging" {
-  crypto_key_id = google_kms_crypto_key.portal.id
+  count         = var.cmek_enabled ? 1 : 0
+  crypto_key_id = one(google_kms_crypto_key.portal[*].id)
   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
   # The data source returns a bare EMAIL; an IAM member needs its principal type. Without the
   # prefix the apply fails with "invalid value ... for member".

infra/terraform/outputs.tf
@@ -24,7 +24,7 @@ output "rollback_image_digests" {
 }
 
 output "cmek_key" {
-  value       = google_kms_crypto_key.portal.id
+  value       = one(google_kms_crypto_key.portal[*].id)
   description = "Regional CMEK bound to Cloud Run revisions and the audit bucket."
 }
 